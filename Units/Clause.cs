f1ca3fa96e08d27960391ff950b0980ac520d790
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-04-21 22:25:12 +0400

    ADD: Clause

--- Units/Clause.cs
+++ Units/Clause.cs
@@ -7,6 +7,25 @@ namespace ManuscriptsProcessor.Units
 	public class Clause : Unit
 	{
 		[JsonProperty]
-		public List<Unit> lesserUnits {get; set;}
+		public List<Realization> realizations {get; set;}
+
+		[JsonConstructor]
+        public Clause(string _clauseID, string _clauseText, List<Dictionary<string, List<Value>>> _clauseFields, List<Realization> _realizations)
+        {
+            id = _clauseID;
+            name = _clauseText;
+            tagging = _clauseFields;
+            realizations = _realizations;
+        }
+        public Clause(string _clauseID, string _clauseText)
+        {
+            id = _clauseID;
+            name = _clauseText;
+        }
+
+        public Clause()
+        {
+
+        }
 	}
 }
---
+++ Units/Realization.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using ManuscriptsProcessor.Values;
+using Newtonsoft.Json;
+
+namespace ManuscriptsProcessor.Units
+{
+	public class Realization : Unit 
+	{
+
+	}
+}
