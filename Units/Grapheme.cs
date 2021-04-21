b8b06553cd2922629a9517f835ddeb3e6df67b44
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-04-21 22:56:50 +0400

    ADD: Grapheme

--- Units/Grapheme.cs
+++ Units/Grapheme.cs
@@ -7,5 +7,24 @@ namespace ManuscriptsProcessor.Units
 	public class Grapheme : Unit 
 	{
 
+		[JsonConstructor]
+        public Grapheme(List<Dictionary<string, List<Value>>> _fields, string _graphemeID, string _grapheme)
+        {
+        	tagging = _fields;
+            id = _graphemeID;
+            name = _grapheme;
+        }        
+        
+        public Grapheme(string _graphemeID, string _grapheme)
+        {           
+            id = _graphemeID;
+            name = _grapheme;
+        }
+
+        public Grapheme()
+        {
+
+        }
+
 	}
 }
