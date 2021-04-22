900c52cbd37e265ebbc7b2b7c02352cf803bc8b3
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-04-22 17:00:52 +0400

    IMPL: Tree structure

--- Fields/Tree.cs
+++ Fields/Tree.cs
@@ -1,12 +1,21 @@
+using System.Collections.Generic;
 using Newtonsoft.Json;
+using ManuscriptsProcessor.Values;
 
 namespace ManuscriptsProcessor.Fields
 {
 	public class Tree
 	{
+		[JsonProperty]
+		public List<Node> nodes {get; set;}
+		[JsonProperty]
+		public List<Connection> connections {get; set;}
+
+		[JsonConstructor]
+
 		public Tree()
 		{
-
+			nodes.Add(new Node() { content = "S", type = "Root"});
 		}
 	}
 }
