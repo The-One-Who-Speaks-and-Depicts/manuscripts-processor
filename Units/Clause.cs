0140b327a03661403fc4ffc6a01d32eb7b3c89e7
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-08-15 22:36:36 +0400

    ADD: ISequenced implementation in Clause

--- Units/Clause.cs
+++ Units/Clause.cs
@@ -5,21 +5,24 @@ using Newtonsoft.Json;
 
 namespace ManuscriptsProcessor.Units 
 {    
-	public class Clause : Unit
+	public class Clause : Unit, ISequenced
 	{
 		[JsonProperty]
 		public List<Token> tokens {get; set;}
         [JsonProperty]
         public Tree parsing {get; set;}
+        [JsonProperty]
+        public string placeInRow {get; set;}
 
 		[JsonConstructor]
-        public Clause(string _clauseID, string _clauseText, List<Tagging> _clauseFields, List<Token> _tokens, Tree _parsing)
+        public Clause(string _clauseID, string _clauseText, List<Tagging> _clauseFields, List<Token> _tokens, Tree _parsing, string _placeInRow)
         {
             Id = _clauseID;
             name = _clauseText;
             tagging = _clauseFields;
             tokens = _tokens;
             parsing = _parsing;
+            placeInRow = _placeInRow;
         }
         public Clause(string _clauseID, string _clauseText)
         {
