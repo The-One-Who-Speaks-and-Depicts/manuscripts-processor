d0718eaa924eeedf416cd18b401fc7a6ba020d50
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-08-15 22:41:31 +0400

    ADD: ISequenced implementation for Token

--- Units/Token.cs
+++ Units/Token.cs
@@ -4,21 +4,24 @@ using Newtonsoft.Json;
 
 namespace ManuscriptsProcessor.Units
 {
-	public class Token : Unit 
+	public class Token : Unit, ISequenced
 	{
 		[JsonProperty]
 		public List<Grapheme> graphemes {get; set;}
 		[JsonProperty]
 		public string shownToken {get; set;}
+        [JsonProperty]
+        public string placeInRow {get; set;}
 
 		[JsonConstructor]
-        public Token(List<Tagging> _fields, string _tokenID, string _lexemeOne, string _lexemeTwo, List<Grapheme> _letters)
+        public Token(List<Tagging> _fields, string _tokenID, string _lexemeOne, string _lexemeTwo, List<Grapheme> _letters, string _placeInRow)
         {            
             tagging = _fields;
             Id = _tokenID;
             name = _lexemeOne;
             shownToken = _lexemeTwo;
             graphemes = _letters;
+            placeInRow = _placeInRow;
         }
         
 
