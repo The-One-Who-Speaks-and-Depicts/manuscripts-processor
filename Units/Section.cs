1ea0802664e4e481001ba8977377fa791856459e
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-08-15 22:39:06 +0400

    ADD: ISequenced implementation in Section

--- Units/Section.cs
+++ Units/Section.cs
@@ -5,18 +5,21 @@ using Newtonsoft.Json;
 namespace ManuscriptsProcessor.Units 
 {
     
-	public class Section : Unit
+	public class Section : Unit, ISequenced
 	{
 		[JsonProperty]
 		public List<Segment> segments {get; set;}
+        [JsonProperty]
+        public string placeInRow {get; set;}
 
 		[JsonConstructor]
-        public Section(string _sectionID, string _sectionName, List<Tagging> _sectionMetaData, List<Segment> _segments)
+        public Section(string _sectionID, string _sectionName, List<Tagging> _sectionMetaData, List<Segment> _segments, string _placeInRow)
         {
             Id = _sectionID;
             name = _sectionName;
             tagging = _sectionMetaData;
             segments = _segments;
+            placeInRow = _placeInRow;
         }
 
         public Section(string _sectionID, string _sectionName)
