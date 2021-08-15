6b7353922e93c6506aae53ea486a5bb3342e93ef
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-08-15 22:37:59 +0400

    ADD: ISequenced implementation in Grapheme

--- Units/Grapheme.cs
+++ Units/Grapheme.cs
@@ -4,15 +4,18 @@ using Newtonsoft.Json;
 
 namespace ManuscriptsProcessor.Units
 {
-	public class Grapheme : Unit 
+	public class Grapheme : Unit, ISequenced
 	{
+        [JsonProperty]
+        public string placeInRow {get; set;}
 
 		[JsonConstructor]
-        public Grapheme(List<Tagging> _fields, string _graphemeID, string _grapheme)
+        public Grapheme(List<Tagging> _fields, string _graphemeID, string _grapheme, string _placeInRow)
         {
         	tagging = _fields;
             Id = _graphemeID;
             name = _grapheme;
+            placeInRow = _placeInRow;
         }        
         
         public Grapheme(string _graphemeID, string _grapheme)
